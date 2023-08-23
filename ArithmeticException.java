0e1e253248135c4b647a857f78526e95bfa5bf75
vishwachaithanya <142490422+vishwachaithanya@users.noreply.github.com>
2023-08-23 21:16:22 +0530

    Updated.java

--- ArithmeticException.java
+++ ArithmeticException.java
@@ -2,6 +2,7 @@ package HandleException11;
 import java.util.Scanner;
 public class ArithmeticException
 {
+	//chaithu
 	public static void main(String[] args)
 	{
 		Scanner s=new Scanner(System.in);
